0c0112733a9099f40f4ed2bc54f4ceb347bf585a
alexanderprygunov <a.a.prygunov@gmail.com>
2016-10-13 22:14:06 +0300

    Исправление ошибок от 07 октября (Compare)

--- lab1/compare/compare/tests.bat
+++ lab1/compare/compare/tests.bat
@@ -1,27 +1,39 @@
 set PROGRAM="%~1"
 
-rem cравнение содержимого stdout
+rem cравнение пустых файлов
+%PROGRAM% empty.txt empty2.txt > "%TEMP%\output.txt"
+if NOT ERRORLEVEL 0 goto error
+fc.exe "%TEMP%\output.txt" empty-result.txt
+if NOT ERRORLEVEL 0 goto error
+
+rem ожидаем код ошибки, не равный нулю, если один файл пуст, а второй нет
+%PROGRAM% empty.txt firstFile.txt > "%TEMP%\output.txt"
+if NOT ERRORLEVEL 1 goto error
+fc.exe "%TEMP%\output.txt" empty-and-not-empty-file.txt
+if NOT ERRORLEVEL 0 goto error
+
+rem ожидаем код ошибки, не равный нулю, если файлы разные
 %PROGRAM% firstFile.txt secondFile.txt > "%TEMP%\output.txt"
 IF NOT ERRORLEVEL 0 goto error
 fc.exe "%TEMP%\output.txt" expected-firstFile-and-secondFile.txt
 if ERRORLEVEL 1 goto error
 
-rem cравнение пустых файлов
-fc.exe empty.txt empty2.txt
-if NOT ERRORLEVEL 0 goto error
-
-rem ожидаем код ошибки, не равный нулю, если один файл пуст, а второй нет
-fc.exe empty.txt firstFile.txt
-if NOT ERRORLEVEL 1 goto error
-
-rem ожидаем код ошибки, не равный нулю, если файлы разные
-fc.exe firstFile.txt secondFile.txt
-if NOT ERRORLEVEL 1 goto error
-
-rem ожидаем код ошибки, не равный нулю, если не найден один из входящих файлов
-%PROGRAM% non-existing-file-name.txt firstFile.txt > "%TEMP%\output.txt"
+rem ожидаем код ошибки, не равный нулю, если не найден один из входящих файлов (программа не смогла открыть файл)
+%PROGRAM% non-existing-file.txt firstFile.txt > "%TEMP%\output.txt"
 if NOT ERRORLEVEL 1 goto error
 fc.exe "%TEMP%\output.txt" expected-output-when-input-file-is-missing.txt
+if ERRORLEVEL 1 goto error
+
+rem проверка программы, если не передано параметров
+%PROGRAM% > "%TEMP%\output.txt"
+if NOT ERRORLEVEL 1 goto error
+fc.exe %TEMP%\output.txt output-parameters-count-error.txt
+if ERRORLEVEL 1 goto error
+
+rem проверка программы, если передан только один параметр
+%PROGRAM% firstFile.txt > "%TEMP%\output.txt"
+if NOT ERRORLEVEL 1 goto error
+fc.exe %TEMP%\output.txt output-parameters-count-error.txt
 if ERRORLEVEL 1 goto error
 
 echo Program testing succeeded
