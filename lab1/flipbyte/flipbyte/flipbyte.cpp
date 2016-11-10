7f8749d8d5830727f528582897a367107b8b9040
alexanderprygunov <a.a.prygunov@gmail.com>
2016-11-10 21:39:04 +0300

    Добавлена программа flipbyte

---
+++ lab1/flipbyte/flipbyte/flipbyte.cpp
@@ -0,0 +1,44 @@
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+void NumberReverseOrderBits(int inputByte)
+{
+	int bitNumber = 0;
+	int outputByte = 0;
+	int twoInDegree = 128;
+	while (bitNumber != 8)
+	{
+		if ((inputByte != 0) && (inputByte & 1))
+		{
+			outputByte += twoInDegree;
+		}
+		inputByte >>= 1;
+		++bitNumber;
+		twoInDegree /= 2;
+	}
+	cout << outputByte << endl;
+}
+
+int main(int argc, char * argv[]) 
+{
+	if (argc != 2)
+	{
+		cout << "Invalid arguments count" << endl <<
+			"Usage: flipbyte.exe <input byte>" << endl;
+		return 1;
+	}
+	 
+	int inputByte = atoi(argv[1]); 
+
+	if (inputByte < 0 || inputByte > 255)
+	{
+		cout << "Number entered should be from 0 to 255" << endl;
+		return 1;
+	}
+
+	NumberReverseOrderBits(inputByte);
+
+	return 0;
+}
---
+++ lab1/flipbyte/flipbyte/tests.bat
@@ -0,0 +1,27 @@
+set PROGRAM="%~1" 
+
+rem проверка числа, после изменения байтов которого ничего не изменится
+%PROGRAM% 0 > output
+fc output zero.txt
+if ERRORLEVEL 1 goto error
+
+rem проверка колличества аргументов
+%PROGRAM%
+if NOT ERRORLEVEL 1 goto error 
+
+rem проверка выхода за пределы
+%PROGRAM% -1 > output
+fc output overflow.txt
+if ERRORLEVEL 1 goto error 
+
+rem проверка оптимального варианта, должно вернуть 96
+%PROGRAM% 6 > output 
+fc output good.txt
+if ERRORLEVEL 1 goto error
+
+echo Program testing succeeded 
+exit 0 
+
+:error 
+echo Program testing failed 
+exit 1
