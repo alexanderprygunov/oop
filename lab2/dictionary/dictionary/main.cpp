306b94c48bb4e8e96ead26b687fdb2568d885e4e
alexanderprygunov <a.a.prygunov@gmail.com>
2016-12-15 00:08:42 +0300

    добавлена программа dictionary

---
+++ lab2/dictionary/dictionary/main.cpp
@@ -0,0 +1,36 @@
+﻿#include "stdafx.h"
+#include "dictionaryProcessor.h"
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	if (argc != 2)
+	{
+		cout << "Invalid count of arguments\n"
+			 << "Usage: dictionary.exe <dictionary file name>\n";
+		return 1;
+	}
+	
+	string dictionaryFileInput = argv[1];
+	Dictionary dictionary;
+	ifstream dictionaryInput(dictionaryFileInput);
+
+	if (dictionaryInput.is_open())
+	{
+		SerializeDictionary(dictionary, dictionaryInput);
+		dictionaryInput.close();
+	}
+	else
+	{
+		cout << "Can't process " << dictionaryFileInput << " for open";
+	}
+
+	stringstream input;
+	ProcessDictionary(dictionary, cin, dictionaryFileInput);
+
+	return 0;
+}
